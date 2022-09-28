172696bd998df35ddf1cf962a73e5c7e67d598a8
jigysha-art <s.jigisha786@gmail.com>
2022-09-28 19:19:39 +0530

    init commit

---
+++ src/design.js
@@ -0,0 +1,21 @@
+/*import React from 'react';
+
+
+function App() {
+    return (
+        <div>
+            <div>
+                X
+            </div>
+            <div>
+                x
+            </div>
+            <div>
+                x
+            </div>
+        </div>
+        
+    );
+}
+
+export default App;*/
